cd0afb9e8a668727165ca825eda5b3754a824102
Daniel S <daniel.solisb@outlook.com>
2025-02-27 11:14:08 -0500

    27-02-2025

--- holaenfermera/users/models.py
+++ holaenfermera/users/models.py
@@ -12,6 +12,10 @@ from django.db.models.fields.files import ImageField
 import datetime
 
 # Create your models here.
+# users/models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 class User(AbstractUser):
     options=[
